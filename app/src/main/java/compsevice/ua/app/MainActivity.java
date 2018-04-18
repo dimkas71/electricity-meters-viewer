0a8668b74d9a32fad69a8fc9815a8d5ce8cd2394
dimkas71g@gmail.com <dimkas71g@gmail.com>
2018-04-18 22:26:31 +0300

    Adding recylcerView to the main Layout

--- app/src/main/java/compsevice/ua/app/MainActivity.java
+++ app/src/main/java/compsevice/ua/app/MainActivity.java
@@ -2,12 +2,22 @@ package compsevice.ua.app;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.ProgressBar;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView recylcerView;
+    private ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //1. ProgressBar
+        progressBar = findViewById(R.id.progress_bar);
+
+        recylcerView = findViewById(R.id.recycler_view);
     }
 }
