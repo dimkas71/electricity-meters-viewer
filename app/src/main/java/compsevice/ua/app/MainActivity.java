47dc7501dc1c2195fbc3cdcb1bccec447482b17e
dimkas71 <dimkas71g@gmail.com>
2018-04-19 13:47:34 +0300

    Adding menu to the app

--- app/src/main/java/compsevice/ua/app/MainActivity.java
+++ app/src/main/java/compsevice/ua/app/MainActivity.java
@@ -1,8 +1,12 @@
 package compsevice.ua.app;
 
+import android.support.v4.view.MenuCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.view.SupportMenuInflater;
 import android.support.v7.widget.RecyclerView;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.widget.ProgressBar;
 
@@ -19,5 +23,11 @@ public class MainActivity extends AppCompatActivity {
         progressBar = findViewById(R.id.progress_bar);
 
         recylcerView = findViewById(R.id.recycler_view);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
     }
 }
