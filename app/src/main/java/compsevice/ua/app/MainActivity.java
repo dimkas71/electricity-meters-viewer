c65d006d6d46aecf123251484bad9b9da714568a
dimkas71g@gmail.com <dimkas71g@gmail.com>
2018-04-21 18:09:42 +0300

    Changing list_item layout, adding fields: contractnumber, sectornumber and owner

--- app/src/main/java/compsevice/ua/app/MainActivity.java
+++ app/src/main/java/compsevice/ua/app/MainActivity.java
@@ -2,19 +2,32 @@ package compsevice.ua.app;
 
 import android.app.SearchManager;
 import android.os.Bundle;
-import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.SearchView;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ProgressBar;
 import android.widget.Toast;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import compsevice.ua.app.adapter.ContractInfoAdapter;
+import compsevice.ua.app.model.ContractInfo;
+
 public class MainActivity extends AppCompatActivity {
 
     private RecyclerView recylcerView;
     private ProgressBar progressBar;
+    private ContractInfoAdapter contractInfoAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,8 +36,58 @@ public class MainActivity extends AppCompatActivity {
 
          //1. ProgressBar
         progressBar = findViewById(R.id.progress_bar);
-
         recylcerView = findViewById(R.id.recycler_view);
+
+        LinearLayoutManager layoutManager = new LinearLayoutManager(getApplicationContext());
+
+        recylcerView.setLayoutManager(layoutManager);
+
+        contractInfoAdapter = new ContractInfoAdapter(getApplicationContext(), fromJson());
+
+        recylcerView.setAdapter(contractInfoAdapter);
+
+
+    }
+
+    private List<ContractInfo> fromJson() {
+
+
+
+
+
+
+
+        //String json = "[{\"id\":95,\"number\":\"121 Р = 1886/2тс ОПР\",\"owner\":\"Панцир Стелла Петрівна\",\"sectorNumber\":2,\"counters\":[{\"id\":105,\"factoryNumber\":\"0603490702535345\",\"value\":2170}],\"credits\":[{\"id\":95,\"service\":\"ELECTRICITY\",\"counter\":\"0603490702535345\",\"credit\":2448.82},{\"id\":95,\"service\":\"VIDEO\",\"counter\":\"\",\"credit\":48.0},{\"id\":95,\"service\":\"SERVICE\",\"counter\":\"\",\"credit\":337.0}]},{\"id\":1052,\"number\":\"121 Р = 3420/В\",\"owner\":\"Зеленюк Василь Миколайович\",\"sectorNumber\":2,\"counters\":[{\"id\":1143,\"factoryNumber\":\"09093205\",\"value\":2152}],\"credits\":[{\"id\":1052,\"service\":\"VIDEO\",\"counter\":\"\",\"credit\":48.0},{\"id\":1052,\"service\":\"SERVICE\",\"counter\":\"\",\"credit\":689.0},{\"id\":1052,\"service\":\"ELECTRICITY\",\"counter\":\"09093205\",\"credit\":-0.55}]}]";
+
+        ObjectMapper mapper = new ObjectMapper();
+
+        List<ContractInfo> infos = new ArrayList<>();
+
+
+        InputStream is = null;
+
+        try {
+
+            is = getResources().openRawResource(R.raw.data);
+
+            ContractInfo[] cis = mapper.readValue(is, ContractInfo[].class);
+            infos.addAll(Arrays.asList(cis));
+            Log.i("Info", infos.toString());
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (is != null) {
+                try {
+                    is.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+
+        return infos;
+
     }
 
     @Override
--- app/src/main/java/compsevice/ua/app/adapter/ContractInfoAdapter.java
+++ app/src/main/java/compsevice/ua/app/adapter/ContractInfoAdapter.java
@@ -1,4 +1,67 @@
-package compsevice.ua.app.compservice.ua.app.adapter;
+package compsevice.ua.app.adapter;
 
-public class ContactInfoAdapter {
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
+
+import compsevice.ua.app.R;
+import compsevice.ua.app.model.ContractInfo;
+
+public class ContractInfoAdapter extends RecyclerView.Adapter<ContractInfoAdapter.ViewHolder> {
+
+
+    private List<ContractInfo> contracts;
+    private Context context;
+
+    public ContractInfoAdapter(Context context, List<ContractInfo> contracts) {
+        this.contracts = contracts;
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+        LayoutInflater inflater = LayoutInflater.from(context);
+        ViewHolder vh = new ViewHolder(inflater.inflate(R.layout.list_item, parent, false));
+        return vh;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        ContractInfo contractInfo = contracts.get(position);
+        holder.tvSectorNumber.setText(context.getResources().getString(R.string.text_sector_number, contractInfo.getSectorNumber()));
+        holder.tvContractNumber.setText(context.getResources().getString(R.string.text_contract_number,
+                                            contractInfo.getNumber()));
+
+        holder.tvOwner.setText(contractInfo.getOwner());
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return contracts.size();
+    }
+
+    static class ViewHolder extends RecyclerView.ViewHolder {
+
+        TextView tvSectorNumber;
+        TextView tvContractNumber;
+        TextView tvOwner;
+
+        public ViewHolder(View parent) {
+            super(parent);
+            tvSectorNumber = parent.findViewById(R.id.text_sector_number);
+            tvContractNumber = parent.findViewById(R.id.text_number);
+            tvOwner = parent.findViewById(R.id.text_owner);
+        }
+    }
+
+
 }
