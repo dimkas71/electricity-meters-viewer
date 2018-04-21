059965784b883a0fcee054c2ea48c595035f9984
dimkas71g@gmail.com <dimkas71g@gmail.com>
2018-04-21 16:25:05 +0300

    Adding SearchView support for the app

--- app/src/main/java/compsevice/ua/app/MainActivity.java
+++ app/src/main/java/compsevice/ua/app/MainActivity.java
@@ -1,14 +1,13 @@
 package compsevice.ua.app;
 
-import android.support.v4.view.MenuCompat;
-import android.support.v7.app.AppCompatActivity;
+import android.app.SearchManager;
 import android.os.Bundle;
-import android.support.v7.view.SupportMenuInflater;
+import android.support.v4.view.MenuItemCompat;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.SearchView;
 import android.view.Menu;
-import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.view.View;
 import android.widget.ProgressBar;
 import android.widget.Toast;
 
@@ -21,7 +20,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //1. ProgressBar
+
+         //1. ProgressBar
         progressBar = findViewById(R.id.progress_bar);
 
         recylcerView = findViewById(R.id.recycler_view);
@@ -30,6 +30,13 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu, menu);
+
+        SearchView searchView = (SearchView) menu.findItem(R.id.action_search).getActionView();
+        SearchManager sm = (SearchManager) getSystemService(SEARCH_SERVICE);
+
+        searchView.setSearchableInfo(sm.getSearchableInfo(getComponentName()));
+
+
         return super.onCreateOptionsMenu(menu);
     }
 
