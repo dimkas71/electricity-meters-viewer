5579dd648781f8f02aa5bead3a5f7bc83daab3cd
dimkas71g@gmail.com <dimkas71g@gmail.com>
2018-05-13 17:08:02 +0300

    Changing ContractInfoDetailActivity with eliminating companion object to module properties and methods and Adding Adapter for ContractInfoDetailActivity

--- app/src/main/java/compsevice/ua/app/activity/ContractInfoDetailActivity.kt
+++ app/src/main/java/compsevice/ua/app/activity/ContractInfoDetailActivity.kt
@@ -2,42 +2,35 @@ package compsevice.ua.app.activity
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.support.v7.widget.RecyclerView
+import android.support.v7.widget.LinearLayoutManager
+import android.text.format.DateFormat
 import compsevice.ua.app.R
+import compsevice.ua.app.adapter.ContractInfoDetailCounterAdapter
+import kotlinx.android.synthetic.main.activity_contract_info_detail.*
 import java.util.*
 
-class ContractInfoDetailActivity() : AppCompatActivity() {
+val COUNTER_THRESHHOLD: Int = 10
+val TAG: String? = ContractInfoDetailActivity::class.simpleName
 
-    val data = mutableListOf<Counter>()
-
-    companion object {
-        val TAG: String? = ContractInfoDetailActivity::class.simpleName;
-        val COUNTER_THRESHHOLD: Int = 10
-    }
-
-    init {
-        data.addAll(Array<Counter>(COUNTER_THRESHHOLD, { n ->
-            Counter(Calendar.getInstance().apply {
-                set(2018, Calendar.MAY, n)
+val data: MutableList<Counter> = (1..COUNTER_THRESHHOLD).map { n ->
+                Counter(Calendar.getInstance().apply {
+                    set(2018, Calendar.MAY, n)
             }, n, 1)
-        }))
-    }
+        }.toMutableList()
 
+data class Counter(val period: Calendar, val value: Int, val delta: Int) {
+    fun periodToString(): String = DateFormat.format("dd.MM.yyyy", period).toString()
+}
 
-    private lateinit var recyclerView: RecyclerView
+class ContractInfoDetailActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_contract_info_detail)
-
-        recyclerView = findViewById<RecyclerView>(R.id.recyclerViewByPeriod)
-
-
-
-
+        recyclerViewByPeriod.layoutManager = LinearLayoutManager(applicationContext)
+        recyclerViewByPeriod.adapter = ContractInfoDetailCounterAdapter(applicationContext, data)
 
     }
-}
 
-data class Counter(val period: Calendar, val value: Int, val delta: Int)
+}
 
