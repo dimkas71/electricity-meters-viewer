d0fa999ec1e6ed4a04c68ecadabca53d12a1676b
dimkas71g@gmail.com <dimkas71g@gmail.com>
2018-05-18 18:03:53 +0300

    Adding inline downloader for demo

--- app/src/main/java/compsevice/ua/app/activity/ContractInfoDetailActivity.kt
+++ app/src/main/java/compsevice/ua/app/activity/ContractInfoDetailActivity.kt
@@ -1,9 +1,13 @@
 package compsevice.ua.app.activity
 
+import android.os.AsyncTask
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
 import android.text.format.DateFormat
+import android.util.Log
+import android.view.View
+import android.widget.Toast
 import compsevice.ua.app.R
 import compsevice.ua.app.adapter.ContractInfoDetailCounterAdapter
 import kotlinx.android.synthetic.main.activity_contract_info_detail.*
@@ -22,6 +26,11 @@ data class Counter(val period: Calendar, val value: Int, val delta: Int) {
     fun periodToString(): String = DateFormat.format("dd.MM.yyyy", period).toString()
 }
 
+fun ContractInfoDetailActivity.toast(message: String): Unit {
+    Toast.makeText(this, message.toString(), Toast.LENGTH_LONG).show()
+}
+
+
 class ContractInfoDetailActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -30,7 +39,32 @@ class ContractInfoDetailActivity : AppCompatActivity() {
         recyclerViewByPeriod.layoutManager = LinearLayoutManager(applicationContext)
         recyclerViewByPeriod.adapter = ContractInfoDetailCounterAdapter(applicationContext, data)
 
+
+        updateButton.setOnClickListener {
+            Downloader().execute(null)
+        }
+
     }
+
+    inner class Downloader : AsyncTask<String, Void, Void>() {
+        override fun onPostExecute(result: Void?) {
+            super.onPostExecute(result)
+            progressBar.visibility = View.GONE
+        }
+
+        override fun doInBackground(vararg params: String?): Void? {
+            Log.i(ContractInfoDetailActivity::class.java.simpleName, "Hello ${params[0] ?: "Nothing"}")
+            return null
+        }
+
+        override fun onPreExecute() {
+            super.onPreExecute()
+            progressBar.visibility = View.VISIBLE
+        }
+
+
+    }
+
 
 }
 
