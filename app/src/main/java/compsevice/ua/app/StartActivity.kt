f3ef4df9cc0677dc30c8c178a7ba80b01085df84
dimkas71g@gmail.com <dimkas71g@gmail.com>
2018-06-04 21:58:18 +0300

    Adding menu to the StartActivity

--- app/src/main/java/compsevice/ua/app/StartActivity.kt
+++ app/src/main/java/compsevice/ua/app/StartActivity.kt
@@ -1,12 +1,33 @@
 package compsevice.ua.app
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.view.LayoutInflaterCompat
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import compsevice.ua.app.activity.SettingsActivity
 
 class StartActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_start)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        return when (item?.itemId) {
+            R.id.action_settings -> {
+                startActivity(Intent(this, SettingsActivity::class.java))
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
