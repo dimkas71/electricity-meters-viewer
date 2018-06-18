5fe438fc5d924e257e4aa86b841fc0506d257c74
dimkas71 <dimkas71g@gmail.com>
2018-06-18 14:59:35 +0300

    Adding fab to the project

--- app/src/main/java/compsevice/ua/app/activity/ContractInfoDetailActivity.kt
+++ app/src/main/java/compsevice/ua/app/activity/ContractInfoDetailActivity.kt
@@ -2,12 +2,14 @@ package compsevice.ua.app.activity
 
 import android.content.Intent
 import android.os.Bundle
+import android.support.design.widget.FloatingActionButton
 import android.support.design.widget.TabLayout
 import android.support.v4.view.ViewPager
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Toast
 import compsevice.ua.app.R
 import compsevice.ua.app.adapter.KEY_CONTRACT
 
@@ -15,7 +17,7 @@ class ContractInfoDetailActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_contract_info_detail)
+        setContentView(R.layout.activity_contract_info_detail_new)
 
 
         val adapter = ViewPagerAdapter(applicationContext, supportFragmentManager)
@@ -31,6 +33,12 @@ class ContractInfoDetailActivity : AppCompatActivity() {
         val contract = intent.extras[KEY_CONTRACT]
 
         Log.i(ContractInfoDetailActivity::class.java.simpleName, "Contract $contract")
+
+        val fab = findViewById<FloatingActionButton>(R.id.fab)
+
+        fab.setOnClickListener {
+           Toast.makeText(it.context, "Hello ", Toast.LENGTH_LONG).show()
+        }
 
     }
 
