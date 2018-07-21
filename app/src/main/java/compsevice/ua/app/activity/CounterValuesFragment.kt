89c018b78d7c09c4bc325f588cd675ae5ae32f90
dimkas71g@gmail.com <dimkas71g@gmail.com>
2018-07-21 18:30:41 +0300

    Adding Counter values history implementation

--- app/src/main/java/compsevice/ua/app/activity/CounterValuesFragment.kt
+++ app/src/main/java/compsevice/ua/app/activity/CounterValuesFragment.kt
@@ -1,13 +1,21 @@
 package compsevice.ua.app.activity
 
+import android.arch.lifecycle.Observer
+import android.arch.lifecycle.ViewModelProviders
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ListAdapter
 import compsevice.ua.app.R
+import compsevice.ua.app.activity.adapter.CounterValuesAdapter
+import compsevice.ua.app.viewmodel.ContractInfoViewModel
+import kotlinx.android.synthetic.main.contract_info_detail_counter_values.*
 
 class CounterValuesFragment : Fragment() {
+
+    private lateinit var model: ContractInfoViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,4 +26,26 @@ class CounterValuesFragment : Fragment() {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.contract_info_detail_counter_values, container, false)
     }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        model = activity?.let {
+            ViewModelProviders.of(it).get(ContractInfoViewModel::class.java)
+        } ?: ContractInfoViewModel()
+
+
+        model.retreiveData().observe(this, Observer {
+           it?.let {
+
+               val items = it.countersHistory.sortedByDescending { it.period }
+               val adapter = CounterValuesAdapter(items)
+               countersHistoryList.setAdapter(adapter)
+           }
+        })
+
+
+
+    }
+
 }
---
+++ app/src/main/java/compsevice/ua/app/activity/adapter/CounterValuesAdapter.kt
@@ -0,0 +1,74 @@
+package compsevice.ua.app.activity.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseExpandableListAdapter
+import android.widget.TextView
+import compsevice.ua.app.R
+import compsevice.ua.app.viewmodel.CounterValuesHistory
+import java.text.SimpleDateFormat
+
+
+class CounterValuesAdapter(val items: List<CounterValuesHistory>) : BaseExpandableListAdapter() {
+
+    private val formatter: SimpleDateFormat = SimpleDateFormat("dd.MM.yy")
+
+    val map: Map<String, List<CounterValuesHistory>>
+            get() = items.groupBy { it.counter }
+
+
+    val groups: Array<String>
+        get() = items.groupBy { it.counter }.keys.toTypedArray()
+
+
+    override fun getGroup(groupPosition: Int): Any =
+        groups[groupPosition]
+
+    override fun isChildSelectable(groupPosition: Int, childPosition: Int): Boolean = true
+
+    override fun hasStableIds(): Boolean = true
+
+    override fun getGroupView(groupPosition: Int, isExpanded: Boolean, convertView: View?, parent: ViewGroup?): View {
+        val view = convertView ?: LayoutInflater.from(parent?.context).inflate(R.layout.contact_info_detail_counter_list_group, parent, false)
+
+        val group = groups[groupPosition]
+
+        val groupTv = view.findViewById<TextView>(R.id.counterItemTitle)
+
+        groupTv.text = group
+
+        return view
+    }
+
+    override fun getChildrenCount(groupPosition: Int): Int = map[groups[groupPosition]]?.size!!
+
+    override fun getChild(groupPosition: Int, childPosition: Int): Any = map[groups[groupPosition]]?.get(childPosition)!!
+
+    override fun getGroupId(groupPosition: Int): Long = groupPosition.toLong()
+
+    override fun getChildView(groupPosition: Int, childPosition: Int, isLastChild: Boolean, convertView: View?, parent: ViewGroup?): View {
+
+        val view = convertView ?: LayoutInflater.from(parent?.context).inflate(R.layout.contact_info_detail_counter_list_child_item, parent, false)
+
+        val periodTv = view.findViewById<TextView>(R.id.counterHistPeriod)
+        val valueTv = view.findViewById<TextView>(R.id.counterHistValue)
+        val consumtionTv = view.findViewById<TextView>(R.id.counterHistConsumption)
+
+
+        val item = map[groups[groupPosition]]?.get(childPosition)
+
+        item?.let {
+            periodTv.text = formatter.format(it.period)
+            valueTv.text = it.value.toString()
+            consumtionTv.text = it.consumption.toString()
+        }
+
+        return view
+
+    }
+
+    override fun getChildId(groupPosition: Int, childPosition: Int): Long = (groupPosition * 1024 + childPosition).toLong()
+
+    override fun getGroupCount(): Int = groups.size
+}
